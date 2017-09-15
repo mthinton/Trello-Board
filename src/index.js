fbaff15b8e30032366b41401ce192830c3cbf4f1
mthintoncoding <mthinton@gmail.com>
2017-09-14 20:40:10 -0400

    Trello Board

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Board from './components/board'
+import './index.css'
+
+ReactDOM.render(<Board title="Trello Organization List" />, document.getElementById('root'))
